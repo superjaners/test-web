e7f22dbe66da6a8ecfe64f19cd89f838cfb8cd16
superjaners <barditz@hotmail.com>
2025-12-27 19:50:44 -0600

    Refactor script.js for improved readability

--- imasdle/script.js
+++ imasdle/script.js
@@ -121,7 +121,7 @@ async function check(userGuess) {
     const characterData = await getDB();
     const guessedCharacter = characterData.find(item => item.name === userGuess);
 
-    await addHintBox(history, `url("imgs/${guessedCharacter.series.toLower()}/${userGuess}.png")`, "rgba(0,0,0,0)");
+    await addHintBox(history, `url("imgs/${guessedCharacter.series.toLowerCase()}/${userGuess}.png")`, "rgba(0,0,0,0)");
 
     let guessStats = [guessedCharacter.bust, guessedCharacter.height, guessedCharacter.weight];
     let todayStats = [bg.a.bust, bg.a.height, bg.a.weight];
@@ -402,3 +402,4 @@ document.addEventListener("keydown", (e) => {
     }
 
 })
+
